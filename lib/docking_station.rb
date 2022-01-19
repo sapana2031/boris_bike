880924cf2771a473d97097bf8748c22cc734f4f5
sapna tavrawala <sapnatavrawala@outlook.com>
2022-01-19 17:10:06 +0000

    challange12

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,14 +1,21 @@
 class DockingStation
   attr_reader :bike
+
     def initialize
-      @bike = ['bike1']
+      @bike = []
     end
   def release_bike
     "Bike released"
   end
 
+  def return_bike
+    "Bike returned to dock"
+  end 
+
   def dock_bike(bike)
     @bike.push(bike)
     "Bike docked"
   end
+
+  
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,16 +1,24 @@
-require './lib/docking_station.rb'
+require "./lib/docking_station.rb"
 
 describe DockingStation do
-  
   it "responds to release_bike" do
     docking_station = DockingStation.new
     expect(docking_station.release_bike).to eq("Bike released")
-  end 
+  end
 
-  it 'can add a new bike' do
+  it "I can return bike I'have hired" do
     docking_station = DockingStation.new
-    docking_station.dock_bike('bike1')
-    expect(docking_station.dock_bike('bike1')).to eq("Bike docked")
+    expect(docking_station.return_bike).to eq ("Bike returned to dock")
+  end
+
+  it "can add a new bike" do
+    docking_station = DockingStation.new
+    docking_station.dock_bike("bike")
+    expect(docking_station.dock_bike("bike")).to eq("Bike docked")
+  end
+
+    it "raises" do
+      expect { raise "No bikes avaliable" }.to raise_error
+    
   end
 end
-
